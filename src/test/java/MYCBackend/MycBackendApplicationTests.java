af0a451706604eb39abfffdc55014af24ed04310
HuyNguyen <ntghuy@gmail.com>
2021-08-05 15:50:04 +0700

    Create template Spring Boot

---
+++ src/test/java/MYCBackend/MycBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package MYCBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
